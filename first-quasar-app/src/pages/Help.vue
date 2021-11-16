64e4f2f90253c662320821b52d0078bea4a39131
Saul Graves <graves.saul@gmail.com>
2021-11-16 17:02:10 -0500

    quasar todo app

--- first-quasar-app/src/pages/Help.vue
+++ first-quasar-app/src/pages/Help.vue
@@ -1,6 +1,16 @@
 <template>
-  <q-page>
-    <h5>Help</h5>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda alias
+      excepturi in, explicabo sed at facilis odit illum ut vel! Cum perspiciatis
+      cumque totam cupiditate tempore necessitatibus distinctio eaque ipsum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda alias
+      excepturi in, explicabo sed at facilis odit illum ut vel! Cum perspiciatis
+      cumque totam cupiditate tempore necessitatibus distinctio eaque ipsum!
+    </p>
   </q-page>
 </template>
 
--- first-quasar-app/src/pages/Todo.vue
+++ first-quasar-app/src/pages/Todo.vue
@@ -1,11 +1,70 @@
 <template>
-  <q-page>
-    <h5>Todo</h5>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white" separator bordered>
+      <q-item
+        v-for="(task, ind) in tasks"
+        :key="ind"
+        @click="task.done = !task.done"
+        clickable
+        :class="{ 'done bg-blue-1': task.done }"
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn
+            flat
+            round
+            dense
+            color="primary"
+            icon="delete"
+            @click.stop="deleteTask(ind)"
+          />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      tasks: [
+        {
+          title: "Groceries",
+          done: false,
+        },
+        {
+          title: "Clean kitchen",
+          done: true,
+        },
+        {
+          title: "Walk dog",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+});
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #8a8989;
+  }
+}
+</style>
