10a71561599c1472b2c53ebeea187f77de6dd7f9
Saul Graves <graves.saul@gmail.com>
2021-11-19 16:51:47 -0500

    Vue bootcamp

--- vue-bootcamp/example_2/index.js
+++ vue-bootcamp/example_2/index.js
@@ -9,10 +9,27 @@ const app = new Vue({
 		error: '',
 		success: '',
 	},
+	computed: {
+		validName() {
+			return !this.formData.name && this.error
+				? 'Please provide a name.'
+				: '';
+		},
+		validEmail() {
+			return !this.formData.email && this.error
+				? 'Please provide an email.'
+				: '';
+		},
+		validMsg() {
+			return !this.formData.message && this.error
+				? 'Please provide a message.'
+				: '';
+		},
+	},
 	methods: {
 		validateForm() {
 			if (!this.formData.name.length || !this.formData.email.length) {
-				return (this.error = 'Please provide your name and email.');
+				return (this.error = 'Errors below.');
 			}
 			this.error = '';
 			this.formData.name = '';
