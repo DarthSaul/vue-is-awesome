7d7df5a5bf53d7ecc3d34f5ffdb3aac879f5d2e4
Saul Graves <graves.saul@gmail.com>
2021-11-19 12:38:59 -0500

    Vue bootcamp

--- vue-bootcamp/example_1/index.js
+++ vue-bootcamp/example_1/index.js
@@ -1,6 +1,22 @@
 const app = new Vue({
 	el: '#app',
 	data: {
-		message: 'Hello, world.',
+		title: 'Learning Vue on Scrimba',
+		desc: 'This is an interactive lesson on how to remove the data from the DOM and put it on the Vue instance.',
+		link: {
+			url: 'https://vuejs.org/',
+			desc: 'Visit Vue',
+		},
+		name: '',
+	},
+	computed: {
+		exclamationTitle() {
+			return this.title + '!!!';
+		},
+		exclamationName() {
+			return this.name + '!';
+		},
 	},
 });
+
+// setTimeout(() => (app.color = 'red'), 2500);
