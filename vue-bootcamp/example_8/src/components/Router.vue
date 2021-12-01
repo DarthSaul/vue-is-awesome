4b9268fe5a1d29a2382e7b1e79d4a94b69f21723
Saul Graves <graves.saul@gmail.com>
2021-12-01 15:13:50 -0500

    Vue bootcamp

--- vue-bootcamp/example_8/src/components/Router.vue
+++ vue-bootcamp/example_8/src/components/Router.vue
@@ -1,9 +1,24 @@
 <template>
-	<div></div>
+	<div>
+		<h1>Router</h1>
+		<!-- <router-view></router-view> -->
+	</div>
 </template>
 
 <script>
-export default {};
+import VueRouter from 'vue-router';
+
+const PageOne = { template: '<div>Page One</div>' };
+
+const routes = [{ path: '/page-one', component: PageOne }];
+
+const router = new VueRouter({
+	routes,
+});
+
+export default {
+	router,
+};
 </script>
 
 <style scoped></style>
