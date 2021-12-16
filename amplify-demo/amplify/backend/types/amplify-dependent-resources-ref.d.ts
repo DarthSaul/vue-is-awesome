2a0fbfb35791f63c2285095de6930b2c075b875e
Saul Graves <graves.saul@gmail.com>
2021-12-16 09:14:38 -0500

    amplify-demo

---
+++ amplify-demo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifydemo226dc5b0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
